3f9a2821d3d328fa56e6d839a333fe95188c4df3
Qualitrix <Qualitrix@Vinothraj-1310>
2024-12-15 12:40:56 +0530

    Commit from tester1 - testclass

---
+++ AppiumDemoProject/src/test/java/tests/testclass.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class testclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("testclass to add this code to new branch");
+	}
+
+}
